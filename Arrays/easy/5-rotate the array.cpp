7e0aeeea857ee1465757ffa47372f4daceb4674e
Arshad laptop <dekhojara72@gmail.com>
2022-09-03 15:13:34 +0530

    running

---
+++ Arrays/easy/5-rotate the array.cpp
@@ -0,0 +1,10 @@
+//?? Rotate by K 
+//** https://takeuforward.org/data-structure/rotate-array-by-k-elements/
+
+//?? Rotate by One 
+//** https://takeuforward.org/data-structure/left-rotate-the-array-by-one/
+
+//** https://leetcode.com/problems/rotate-array/discuss/1730142/JavaC%2B%2BPython-A-very-very-well-detailed-explanation
+
+
+//**1 Reversal Algo is best of the problem
